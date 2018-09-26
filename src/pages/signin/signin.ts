af0e90db2d19fafa4f36db49c57b8cc65ca85b15
Arjun Kalburgi <askalburgi@gmail.com>
2018-09-26 17:55:17 -0400

    project push

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { GroupPage } from '../group/group';
+import { AuthService } from '../../services/auth.service';
 
 /**
  * Generated class for the SigninPage page.
@@ -13,19 +14,25 @@ import { GroupPage } from '../group/group';
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html',
+  // providers: [SigninPage, AuthService]
 })
 export class SigninPage {
 
   data = { nickname: "" };
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SigninPage');
   }
 
-  enterNickname() {
+  loginWithGoogle() {
+    // this.auth.signInWithGoogle()
+    //   .then(() => this.navCtrl.setRoot(GroupPage),
+    //     error => console.log(error.message)
+    //   );
     this.navCtrl.setRoot(GroupPage, {
       nickname: this.data.nickname
     });
